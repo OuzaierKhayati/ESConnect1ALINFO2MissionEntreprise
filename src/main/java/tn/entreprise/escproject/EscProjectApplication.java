c0d26ef8058dbed490b0bd68384839a292f3c4d0
Ouzaier Khayati <Ouzakhayati@gmail.com>
2026-05-03 10:43:08 +0100

    feat(backend): add initial backend setup with Spring Boot

---
+++ src/main/java/tn/entreprise/escproject/EscProjectApplication.java
@@ -0,0 +1,13 @@
+package tn.entreprise.escproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscProjectApplication.class, args);
+    }
+
+}
